4e3abe95763af3a7f92c046f0d3647647c70f678
RJGY <rjgy2yu@gmail.com>
2022-03-04 00:50:47 +1100

    mouse commands

---
+++ Assets/_Scripts/PlayerMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    void MoveToPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+        {
+            PlayerMoved?.Invoke(hit.point);
+        }
+    }
+}
