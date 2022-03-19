64c43546ad9772cdb8b266ba4f9877f8d80cb113
RJGY <rjgy2yu@gmail.com>
2022-03-20 01:34:33 +1100

    more boiler

---
+++ Assets/_Scripts/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    #region Events
+    public delegate void DealDamage(float damage, Entity enemy, DamageTypes type);
+    public DealDamage DamageDealt;
+    #endregion
+
+    #region Variables
+    private float speed;
+    private float damage;
+    private LayerMask enemyLayer;
+    private DamageTypes type;
+    #endregion
+
+    #region Monobehaviour
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!(collision.gameObject.layer == enemyLayer))
+            return;
+
+        Entity enemy = collision.gameObject.GetComponent<Entity>();
+        DamageDealt?.Invoke(damage, enemy, type);
+    }
+    #endregion
+
+    #region Functions
+
+    #endregion
+}
